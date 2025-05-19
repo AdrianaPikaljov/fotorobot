a7c6e882e8e04c98dab6a9254f37ce6f29372d6c
opilane <opilane@HP-CZC2349HTZ>
2025-05-19 15:22:51 +0300

    dfsfs

--- Fotorobot/Fotorobot.py
+++ Fotorobot/Fotorobot.py
@@ -10,7 +10,7 @@ olemas = {}
 
 silmad_pildid = ["silmad1.png", "silmad2.png", "silmad3.png", "silmad4.png", "silmad5.png"]
 nina_pildid = ["nina1.png", "nina2.png", "nina3.png", "nina4.png"]
-suu_pildid = ["suu1.png", "suu2.png", "suu3.png"]
+suu_pildid = ["suu1.png", "suu2.png", "suu3.png", "suu4.png", "suu5.png", "suu6.png"]
 
 silmad_index = [0]
 nina_index = [0]
@@ -26,46 +26,7 @@ def toggle_osa(nimi, fail, x, y):
         pildid[nimi] = tk_img
         objektid[nimi] = canvas.create_image(x, y, image=tk_img)
         olemas[nimi] = True
-
-def toggle_silmad():
-    nimi = "silmad"
-    if olemas.get(nimi):
-        canvas.delete(objektid[nimi])
-        olemas[nimi] = False
-    fail = silmad_pildid[silmad_index[0]]
-    pil_img = Image.open(fail).convert("RGBA").resize((400, 400))
-    tk_img = ImageTk.PhotoImage(pil_img)
-    pildid[nimi] = tk_img
-    objektid[nimi] = canvas.create_image(200, 200, image=tk_img)
-    olemas[nimi] = True
-    silmad_index[0] = (silmad_index[0] + 1) % len(silmad_pildid)
-
-def toggle_nina():
-    nimi = "nina"
-    if olemas.get(nimi):
-        canvas.delete(objektid[nimi])
-        olemas[nimi] = False
-    fail = nina_pildid[nina_index[0]]
-    pil_img = Image.open(fail).convert("RGBA").resize((400, 400))
-    tk_img = ImageTk.PhotoImage(pil_img)
-    pildid[nimi] = tk_img
-    objektid[nimi] = canvas.create_image(200, 200, image=tk_img)
-    olemas[nimi] = True
-    nina_index[0] = (nina_index[0] + 1) % len(nina_pildid)
-
-def toggle_suu():
-    nimi = "suu"
-    if olemas.get(nimi):
-        canvas.delete(objektid[nimi])
-        olemas[nimi] = False
-    fail = suu_pildid[suu_index[0]]
-    pil_img = Image.open(fail).convert("RGBA").resize((400, 400))
-    tk_img = ImageTk.PhotoImage(pil_img)
-    pildid[nimi] = tk_img
-    objektid[nimi] = canvas.create_image(200, 200, image=tk_img)
-    olemas[nimi] = True
-    suu_index[0] = (suu_index[0] + 1) % len(suu_pildid)
-
+    
 def mängi_muusika():
     pygame.mixer.music.play(loops=-1)
 
@@ -93,7 +54,7 @@ def salvesta_nägu():
     lõpp_pilt.save(f"{failinimi}.png")
     showinfo("Pilt salvestatud", f"Faili nimi on {failinimi}.png")
 
-# GUI setup
+
 pygame.mixer.init()
 pygame.mixer.music.load("Betsy, Ely Oaks & Levinia - Sigma Boy (Lyrics) [Letra].mp3")
 
@@ -107,6 +68,16 @@ canvas.pack(side="right", padx=10, pady=10)
 toggle_osa("nägu", "alus.png", 200, 200)
 olemas["nägu"] = True
 
+toggle_osa("silmad", silmad_pildid[0], 200, 200)
+olemas["silmad"] = True
+
+toggle_osa("nina", nina_pildid[0], 200, 200)
+olemas["nina"] = True
+
+toggle_osa("suu", suu_pildid[0], 200, 200)
+olemas["suu"] = True
+ 
+
 frame = ctk.CTkFrame(app)
 frame.pack(side="left", padx=10, pady=10)
 
@@ -118,10 +89,12 @@ nuppu_seaded = {
     "corner_radius": 20
 }
 
-ctk.CTkLabel(frame, text="Vali näoosad:", **nuppu_seaded).pack(pady=5)
-ctk.CTkButton(frame, text="Silmad", command=toggle_silmad, **nuppu_seaded).pack(pady=3)
-ctk.CTkButton(frame, text="Nina", command=toggle_nina, **nuppu_seaded).pack(pady=3)
-ctk.CTkButton(frame, text="Suu", command=toggle_suu, **nuppu_seaded).pack(pady=3)
+ctk.CTkLabel(frame, text="Vali näoosad:", **nuppu_seaded). pack(pady=5)
+ctk.CkButton(frame, text="Silmad", command=lambda: toggle_osa("silmad","silmad.png", 200, 200), **nuppu_seaded). pack(pady=3)
+ctk.CTkButton(frame, text=" Nina", command=lambda: toggle_osa("nina", "nina.png", 200, 200), **nuppu_seaded) .pack(pady=3)
+ctk.CTkButton(frame, text="Suu", command=lambda: toggle_osa("suu", "suu.png", 200, 200), **nuppu_seaded). pack(pady=3)
+nupp = ctk.CTkButton(frame, text="Salvesta nägu", command=salvesta_nägu, **nuppu_seaded)
+nupp.pack(pady=10)
 
 ctk.CTkButton(frame, text="Salvesta nägu", command=salvesta_nägu, **nuppu_seaded).pack(pady=10)
 
